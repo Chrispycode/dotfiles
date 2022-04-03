41af313e60c77be616ff5f4d9c5c68a2bf9d940a
chrispycode <christopherpauli@gmx.de>
2022-04-03 14:21:44 +0200

    Add install and tilix.conf

---
+++ install.sh
@@ -0,0 +1,10 @@
+# important
+pacman -Sy brave-browser tilix junction rbenv visual-studio-code-bin btop
+# tilix create shortcut with "tilix --quake" command
+dconf load /com/gexperts/Tilix/ < ./tilix.dconf
+ln -s .zshrc ~/.zshrc
+# additional from snap: beekeeper-studio
+# shell extensions: blur-my-shell, Appindicator and KS..., color picker, Extension List, Lock Keys, system-monitor-next 
+
+# fancy
+pacman -Sy boxes zeal copyq appimagelauncher flat-remix flat-remix-gnome flat-remix-gtk tela-circle-icon-theme-git
