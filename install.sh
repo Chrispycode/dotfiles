d269daa4b4874978077739b9c104f74f58bfbf49
chrispycode <christopherpauli@gmx.de>
2022-04-03 17:02:03 +0200

    Add zsh plugins

--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@ sudo pacman -Sy brave-browser tilix btop copyq appimagelauncher gcc python-pip
 sudo pip3 install xkeysnail 
 /bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh || curl -fsSL https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh)"
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 export DOTS="$HOME/repos/dotfiles"
 rm "$HOME/.zshrc"
 rm "$HOME/.config/kinto/kinto.py"
+ln -s "$DOTS/.p10k.zsh" "$HOME/.p10k.zsh"
 ln -s "$DOTS/.zshrc" "$HOME/.zshrc"
 ln -s "$DOTS/kinto.py" "$HOME/.config/kinto"
 
