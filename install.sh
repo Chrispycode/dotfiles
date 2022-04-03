b8c59efb0e07bc60c1c0355917446e662b7460c0
chrispycode <christopherpauli@gmx.de>
2022-04-03 14:52:58 +0200

    ad

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
-# important
-pacman -Sy brave-browser tilix junction rbenv visual-studio-code-bin btop
+#!/usr/bin/env zsh
+
+sudo pacman -Sy brave-browser tilix btop copyq appimagelauncher gcc python-pip
+sudo pip3 install xkeysnail 
+/bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh || curl -fsSL https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh)"
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+export DOTS="$HOME/repos/dotfiles"
+rm "$HOME/.zshrc"
+rm "$HOME/.config/kinto/kinto.py"
+ln -s "$DOTS/.zshrc" "$HOME/.zshrc"
+ln -s "$DOTS/kinto.py" "$HOME/.config/kinto"
+
 # tilix create shortcut with "tilix --quake" command
 dconf load /com/gexperts/Tilix/ < ./tilix.dconf
-ln -s .zshrc ~/.zshrc
-# additional from snap: beekeeper-studio
-# shell extensions: blur-my-shell, Appindicator and KS..., color picker, Extension List, Lock Keys, system-monitor-next 
+gsettings set com.gexperts.Tilix.Settings quake-height-percent 100
 
-# fancy
-pacman -Sy boxes zeal copyq appimagelauncher flat-remix flat-remix-gnome flat-remix-gtk tela-circle-icon-theme-git
+# shell extensions: blur-my-shell, Appindicator and KS..., color picker, Extension List, Lock Keys, system-monitor-next 
+# from gui
+# rbenv visual-studio-code-bin junction boxes zeal flat-remix flat-remix-gnome flat-remix-gtk tela-circle-icon-theme-git beekeeper-studio
